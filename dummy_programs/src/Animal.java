a288fae5e925299f9ef9d6cece32c8cc1c637e6a
nvhiii <nahikhan16@gmail.com>
2022-12-19 16:58:20 -0500

    notes + some code

---
+++ dummy_programs/src/Animal.java
@@ -0,0 +1,17 @@
+public class Animal {
+
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    // Above is a getter method, returns type of String bc name is a String
+
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    // Above is a setter method, void return type bc nothing is returned
+
+}
