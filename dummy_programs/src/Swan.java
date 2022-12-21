021caf3e3a48887f3c5a40bb1aac9576b28ec34c
nvhiii <nahikhan16@gmail.com>
2022-12-20 19:59:21 -0500

    ending on text blocks, continue in morning

---
+++ dummy_programs/src/Swan.java
@@ -0,0 +1,15 @@
+public class Swan {
+
+    int numberEggs; // instance var
+
+    public static void main(String[] args) {
+
+        Swan mother = new Swan();
+
+        mother.numberEggs = 1; // set var
+
+        System.out.println(mother.numberEggs);  // read var
+
+    }
+
+}
