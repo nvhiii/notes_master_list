f0153713c6c890461ebb42d6a8930502da3f88e6
nvhiii <nahikhan16@gmail.com>
2022-12-22 21:11:37 -0500

    added notes up till end of c1, work on this at dawn

---
+++ dummy_programs/src/textBlockPractice.java
@@ -0,0 +1,12 @@
+public class textBlockPractice {
+/*
+    String textBlock = """ // beginning syntax, must have line break here at initialization
+        "Nice"
+        """; // this line indicates extra line, because this could have been placed in line 4 as well
+                therefore, it is essential whitespace (new line char)
+
+
+    }
+
+ */
+}
---
+++ dummy_programs/src/typeInference.java
@@ -0,0 +1,12 @@
+public class typeInference {
+
+    public void whatTypeAmI() {
+
+        var name = "Hello";
+        var size = 7;
+
+    }
+
+    // local variable type inference (doesn't work for class / instance var)
+
+}
